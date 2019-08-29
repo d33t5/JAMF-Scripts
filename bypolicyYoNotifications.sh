a255f8394cb248dcde6908a21d58ec72ed37f5ef
David Tommey <david.tommey@arm.com>
2019-08-29 08:29:15 -0500

    Added workaround for Yo only prompting once

--- bypolicyYoNotifications.sh
+++ bypolicyYoNotifications.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #
 # SYNOPSIS - How to use
 #	Run via a policy to populate Yo with values to present messages to the user.
@@ -10,16 +9,23 @@
 #   Pass in values into these parameters during a policy.
 #
 
-loggedInUser=$( /usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | /usr/bin/awk -F': ' '/[[:space:]]+Name[[:space:]]:/ { if ( $2 != "loginwindow" ) { print $2 }}   ' )
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+
 title=""			#	"string"
 information=""		#	"string"
 button=""			#	"string"
 action=""			#	path
-
 
 [ "$4" != "" ] && [ "$title" == "" ] && title=$4
 [ "$5" != "" ] && [ "$information" == "" ] && information=$5
 [ "$6" != "" ] && [ "$button" == "" ] && button=$6
 [ "$7" != "" ] && [ "$action" == "" ] && action=$7
 
-su $loggedInUser -c '/usr/local/bin/yo_scheduler -t "$title" -n "$information" -b "$button" -a "$action"'
+# Clear any pending notifications
+/usr/local/bin/yo_scheduler --cleanup
+
+# Remove prefs to get around the one time notification issue
+rm /Library/Preferences/com.sheagcraig.yo.plist
+rm /Users/$loggedInUser/Library/Preferences/com.sheagcraig.yo.plist
+
+/usr/local/bin/yo_scheduler -t "$title" -n "$information" -b "$button" -a "$action"
