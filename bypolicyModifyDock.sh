c2d0a21944f82eef679dd9b7977d3000d68c116f
David Tommey <david.tommey@arm.com>
2019-08-29 08:26:10 -0500

    Update loggedInUser command

--- bypolicyModifyDock.sh
+++ bypolicyModifyDock.sh
@@ -11,7 +11,7 @@
 #
 
 dockutilapp=/usr/local/bin/dockutil
-loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 
 fullpathofapptoadd=""
 whichend=""
@@ -20,17 +20,19 @@ dockitemname=""
 [ "$4" != "" ] && [ "$fullpathofapptoadd" == "" ] && fullpathofapptoadd=$4
 [ "$5" != "" ] && [ "$dockitemname" == "" ] && dockitemname=$5
 
+# Install dockutil if it doesn't exist
 if [ ! -f "$dockutilapp" ]; then
   jamf policy -trigger main_dockutil
 fi
 
 dock_item_find=$(sudo -u $loggedInUser $dockutilapp --find "$dockitemname")
 
-if [[ $dock_item_find == *was\ found* ]]; then
+if [[ "$dock_item_find" == *was\ found* ]]
+    then
         #dock item is found
         echo "Skipping '$dockitemname' (Item already in dock)."
     else
         #dock item is not found
-        echo "$dockitemname not in dock. Adding to beginning..."
+        echo "'$dockitemname' not in dock. Adding to beginning..."
         $dockutilapp --add "$fullpathofapptoadd" --position beginning /Users/$loggedInUser
 fi
